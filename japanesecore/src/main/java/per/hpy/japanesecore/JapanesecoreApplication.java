a2f8897b468d7e63b21ed9846631b2d699d3531c
pengyu.huang <pengyu.huang@zuolin.com>
2020-01-22 09:35:40 +0800

    日期

--- japanesecore/src/main/java/per/hpy/japanesecore/JapanesecoreApplication.java
+++ japanesecore/src/main/java/per/hpy/japanesecore/JapanesecoreApplication.java
@@ -1,9 +1,13 @@
 package per.hpy.japanesecore;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("per.hpy.word")
+@ComponentScan(value = {"per.hpy"})
 public class JapanesecoreApplication {
 
     public static void main(String[] args) {
